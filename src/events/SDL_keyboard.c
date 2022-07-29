a620414491bcdbe7334ea636ee33b2e172d5e8f0
Pavel Gorev <pavel.gorev@opera.com>
2022-07-29 10:03:08 +0200

    Change the way of processing virtual keys without additional logic

--- src/events/SDL_keyboard.c
+++ src/events/SDL_keyboard.c
@@ -343,6 +343,7 @@ static const SDL_Keycode SDL_default_keymap[SDL_NUM_SCANCODES] = {
     /* 288 */ SDLK_SOFTRIGHT,
     /* 289 */ SDLK_CALL,
     /* 290 */ SDLK_ENDCALL,
+    SDLK_VIRTUAL_v,
 };
 
 static const char *SDL_scancode_names[SDL_NUM_SCANCODES] = {
@@ -637,6 +638,7 @@ static const char *SDL_scancode_names[SDL_NUM_SCANCODES] = {
     /* 288 */ "SoftRight",
     /* 289 */ "Call",
     /* 290 */ "EndCall",
+    "V"
 };
 
 /* Taken from SDL_iconv() */
